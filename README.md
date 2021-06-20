cdb2bf28a63243740f72347cb73d8d6e066cd928
amenzo <amenze2021@gmail.com>
2021-06-20 13:42:11 +0400

    everything is up to date

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # eritreanFlag
-This project will draw eritrean flag using python!!!!
+This project will draw Eritrean flag using python!!!!
+
+The package imported is turtle.
+
+The steps to draw the flag are finishing:
+	
+	1. the blue portion using draw_blue()
+	2. the red section using draw_red()
+	3. the green section using draw_green()
+	4. the yellow section by finishing the down,left,right and middle section using their respective functions.
+	5. the pole is drawn using draw_pole()
+	
--- flag.py
+++ flag.py
@@ -1,129 +1,149 @@
 import turtle
-turtle.speed(10)
-turtle.color("blue")
-turtle.begin_fill()
-turtle.fd(500)
-turtle.left(90)
-turtle.fd(125)
-turtle.setpos(0,0)
-turtle.end_fill()
 
-#red color
-turtle.color("red")
-turtle.begin_fill()
-turtle.fd(250)
-turtle.setpos(500,125)
-turtle.setpos(0,0)
-turtle.end_fill()
+#turtle.speed(10)
+turtle.fd(0)
+x=input("Press Enter to start")
+yellow_pos=(110,55)
 
-#green color
-turtle.setpos(0,250)
-turtle.color("green")
-turtle.begin_fill()
-turtle.right(90)
-turtle.fd(500)
-turtle.right(90)
-turtle.fd(125)
-turtle.setpos(0,250)
-turtle.end_fill()
-
-#yellow color
-turtle.color("red")
-turtle.setpos(110,55)
-yellow_pos=turtle.pos()
-turtle.color("yellow")
-turtle.begin_fill()
-turtle.setheading(0)
-turtle.right(30)
-turtle.fd(15)
-turtle.setheading(180)
-turtle.fd(26)
-turtle.setpos(yellow_pos)
-list2=turtle.pos()
-angle=0
-turtle.end_fill()
-i=20
-j=0
-k=0
-turtle.setheading(0)
-turtle.circle(70)
-
-while(j<7):
-	
-	list=list2
-	turtle.setheading(angle)
-	turtle.setpos(list)
+def draw_blue():
+	turtle.color("blue")
 	turtle.begin_fill()
-	turtle.circle(98,30)
-	turtle.setheading(190+angle)
-	turtle.fd(i)
-	i+=3
-	list2=turtle.pos()
-	print(list2)
-	turtle.setpos(list)
-	angle+=17
+	turtle.fd(500)
+	turtle.left(90)
+	turtle.fd(125)
+	turtle.setpos(0,0)
 	turtle.end_fill()
-	turtle.dot("black")
-	j+=1
-	k+=3
 
-angle=0
-turtle.color("red")
-turtle.setpos(yellow_pos)
-turtle.color("yellow")
-list2=turtle.pos()
-i=10
-j=0
-
-while(j<7):
-	
-	list=list2
-	turtle.setheading(180-angle)
-	turtle.setpos(list)
+def draw_red():
+	turtle.color("red")
 	turtle.begin_fill()
-	turtle.circle(-98,30)
-	turtle.setheading(-10-angle)
-	turtle.fd(i)
-	i+=1
-	list2=turtle.pos()
-	print(list2)
-	turtle.setpos(list)
-	angle+=15
+	turtle.fd(250)
+	turtle.setpos(500,125)
+	turtle.setpos(0,0)
 	turtle.end_fill()
-	turtle.dot("black")
-	j+=1
 
-turtle.end_fill()
-
-
-turtle.setpos(yellow_pos)
-turtle.setheading(90)
-j=0
-
-while(j<0):
-	turtle.fd(30)
-	list=turtle.pos
+def draw_green():
+	turtle.setpos(0,250)
+	turtle.color("green")
 	turtle.begin_fill()
-	if(j%2==0):
-		turtle.circle(35,80)
-		turtle.setheading(190)
-		turtle.fd(15)
+	turtle.right(90)
+	turtle.fd(500)
+	turtle.right(90)
+	turtle.fd(125)
+	turtle.setpos(0,250)
+	turtle.end_fill()
+
+
+def draw_yellow_down():
+	turtle.color("red")
+	turtle.setpos(110,55)
+	yellow_pos=turtle.pos()
+	turtle.color("yellow")
+	turtle.begin_fill()
+	turtle.setheading(0)
+	turtle.right(15)
+	turtle.fd(45)
+	turtle.setheading(180)
+	turtle.fd(80)
+	turtle.setpos(yellow_pos)
+	turtle.end_fill()
+
+
+
+def draw_yellow_right():
+	list2=turtle.pos()
+	angle=0
+	i=20
+	j=0
+	turtle.setheading(0)
+	while(j<8):
+		
+		list=list2
+		turtle.setheading(angle)
 		turtle.setpos(list)
-	else:
-		turtle.circle(-35,80)
-		turtle.setheading(-10)
-		turtle.fd(15)
+		turtle.begin_fill()
+		turtle.circle(98,30)
+		turtle.setheading(190+angle)
+		turtle.fd(i)
+		i+=3
+		list2=turtle.pos()
 		turtle.setpos(list)
-	tutle.end_fill()
-	j+=1
+		angle+=17
+		turtle.end_fill()
+		j+=1
+		
+
+def draw_yellow_left():
+	angle=0
+	turtle.color("red")
+	turtle.setpos(yellow_pos)
+	turtle.color("yellow")
+	list2=turtle.pos()
+	i=20
+	j=0
+	while(j<8):
+		list=list2
+		turtle.setheading(180-angle)
+		turtle.setpos(list)
+		turtle.begin_fill()
+		turtle.circle(-98,30)
+		turtle.setheading(-10-angle)
+		turtle.fd(i)
+		i+=3
+		list2=turtle.pos()
+		turtle.setpos(list)
+		angle+=17
+		turtle.end_fill()
+		j+=1
 
 
+def draw_yellow_middle():
+	turtle.color("red")
+	turtle.setpos(yellow_pos)
+	turtle.color("yellow")
+	turtle.setheading(90)
+	j=0
+	turtle.pensize(3)
+	while(j<6):
+		turtle.setheading(90)
+		turtle.fd(15)
+		list=turtle.pos()
+		turtle.begin_fill()
+		if(j%2==0):
+			turtle.setheading(0)
+			turtle.circle(35,90)
+			turtle.setheading(190)
+			turtle.fd(15)
+			turtle.setpos(list)
+		else:
+			turtle.setheading(180)
+			turtle.circle(-35,90)
+			turtle.setheading(-10)
+			turtle.fd(15)
+			turtle.setpos(list)
+		turtle.end_fill()
+		j+=1
+
+def draw_pole():
+	turtle.penup()
+	turtle.home()
+	turtle.pendown()
+	turtle.pensize(10)
+	turtle.pencolor("black")
+	turtle.setheading(90)
+	turtle.fd(300)
+	turtle.setheading(-90)
+	turtle.fd(600)
+	turtle.ht()
+
+draw_blue()
+draw_red()
+draw_green()
+draw_yellow_down()	
+draw_yellow_right()
+draw_yellow_left()
+draw_yellow_middle()
+draw_pole()
 
 
-
-
-
-
-
-turtle.ht()
-x=input()
+x=input("Press Enter to close")
